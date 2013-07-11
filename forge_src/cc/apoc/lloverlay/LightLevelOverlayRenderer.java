269f30fdbacbfed6242cb8eb123abff5b5f76eb1
Matthias H <apoc@sixserv.org>
2013-07-12 00:25:43 +0200

    updated to latest forge

--- forge_src/cc/apoc/lloverlay/LightLevelOverlayRenderer.java
+++ forge_src/cc/apoc/lloverlay/LightLevelOverlayRenderer.java
@@ -10,7 +10,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.resources.ResourceLocation;
+import net.minecraft.util.ResourceLocation;
 
 public class LightLevelOverlayRenderer {
     private static class BlockOverlay {
